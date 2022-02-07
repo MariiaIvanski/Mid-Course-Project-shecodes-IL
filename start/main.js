664952172546b2ec8cb227f55808c9267b7273e3
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-07 08:58:16 +0200

    update main.js with given data for task

--- start/main.js
+++ start/main.js
@@ -1,3 +1,92 @@
+"use strict";
+
+(function () {
+  const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+  const apiKey = "39c590036b490e160a1ac1b35a02652d"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+  const activities = {
+    teamIn: ["basketball", "hockey", "volleyball"],
+    teamOutWarm: [
+      "softball/baseball",
+      "football/soccer",
+      "American football",
+      "rowing",
+      "tennis",
+      "volleyball",
+      "ultimate frisbee",
+      "rugby",
+    ],
+    teamOutCold: ["hockey"],
+    soloIn: ["rock climbing", "swimming", "ice skating"],
+    soloOutWarm: ["rowing", "running", "hiking", "cycling", "rock climbing"],
+    soloOutCold: [
+      "snowshoeing",
+      "downhill skiing",
+      "cross-country skiing",
+      "ice skating",
+    ],
+  };
+  let state = {};
+  let category = "all";
+
+  // get weather data when user clicks Forecast button, then add temp & conditions to view
+  document.querySelector(".forecast-button").addEventListener(
+    "click",
+    function (e) {
+      e.preventDefault();
+      document.querySelector("#location").value = "";
+      updateUIFailure();
+    },
+    false
+  );
+
+  // handle ajax failure
+  function updateUIFailure() {
+    document.querySelector(".conditions").textContent =
+      "Weather information unavailable";
+  }
+})();
+
+/*
+function displayActivities(response) {
+  let apiKey = "39c590036b490e160a1ac1b35a02652d";
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let activitiesСhoice = document.querySelector(`#activities`);
+  const activities = {
+    teamIn: ["basketball", "hockey", "volleyball"],
+    teamOutWarm: [
+      "softball/baseball",
+      "football/soccer",
+      "American football",
+      "rowing",
+      "tennis",
+      "volleyball",
+      "ultimate frisbee",
+      "rugby",
+    ],
+    teamOutCold: ["hockey"],
+    soloIn: ["rock climbing", "swimming", "ice skating"],
+    soloOutWarm: ["rowing", "running", "hiking", "cycling", "rock climbing"],
+    soloOutCold: [
+      "snowshoeing",
+      "downhill skiing",
+      "cross-country skiing",
+      "ice skating",
+    ],
+  };
+  let state = {};
+  let category = "all";
+  activitiesСhoice.innerHTML = `<h3>Activities</h3>
+        <div class="activities-list row m-0">
+          <button class="col">Solo</button><button class="col">Team</button
+          ><button class="col">All</button>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -12,64 +101,26 @@ function displayTemperature(response) {
     Math.round(fahrenheitDegrees) +
     ` °F`;
   cityElement.innerHTML = response.data.name;
-
-  function displayActivities(response) {
-    let activitiesСhoice = document.querySelector(`#activities`);
-    const activities = {
-      teamIn: ["basketball", "hockey", "volleyball"],
-      teamOutWarm: [
-        "softball/baseball",
-        "football/soccer",
-        "American football",
-        "rowing",
-        "tennis",
-        "volleyball",
-        "ultimate frisbee",
-        "rugby",
-      ],
-      teamOutCold: ["hockey"],
-      soloIn: ["rock climbing", "swimming", "ice skating"],
-      soloOutWarm: ["rowing", "running", "hiking", "cycling", "rock climbing"],
-      soloOutCold: [
-        "snowshoeing",
-        "downhill skiing",
-        "cross-country skiing",
-        "ice skating",
-      ],
-    };
-    let state = {};
-    let category = "all";
-    activitiesСhoice.innerHTML = `<h3>Activities</h3>
-        <div class="activities-list row m-0">
-          <button class="col">Solo</button><button class="col">Team</button
-          ><button class="col">All</button>
-        </div>
-        <div>
-          <ul>
-            <li></li>
-          </ul>
-        </div>`;
-  }
   displayActivities(celsiusTemperature);
+}
 
-  document.querySelector("#forecast-button").addEventListener(
-    "click",
-    function (e) {
-      e.preventDefault();
-      document.querySelector("#location").value = "";
-      updateUIFailure();
-    },
-    false
-  );
+document.querySelector("#forecast-button").addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault();
+    document.querySelector("#location").value = "";
+    updateUIFailure();
+  },
+  false
+);
 
-  function updateUIFailure() {
-    document.querySelector(".conditions").textContent =
-      "Weather information unavailable";
-  }
+function updateUIFailure() {
+  document.querySelector(".conditions").textContent =
+    "Weather information unavailable";
 }
 
 function search(city) {
-  let apiKey = "a2d283df905dedf8786b96ad24673f92";
+  let apiKey = "39c590036b490e160a1ac1b35a02652d";
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(weatherUrl).then(displayTemperature);
 }
@@ -88,3 +139,4 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+*/

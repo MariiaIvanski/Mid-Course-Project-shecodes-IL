8d423fd43930266a44f1ea134aad5ee10b4cc9b0
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-20 09:05:40 +0200

    install axios

--- start/main.js
+++ start/main.js
@@ -36,7 +36,8 @@
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
       document.querySelector(`#location`).value = ``;
 
-      fetch(url)
+      axios
+        .get(url)
         .then(function (response) {
           return response.json();
         })
